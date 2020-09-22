154ef212f54f2517d0295a7428a07961bde367a1
Levant <1980468272@qq.com>
2020-09-22 15:50:11 +0800

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Helloworld");
+	    int a=2;
+	    int b=3;
+	    int c;
+	    c=a+b;
+        System.out.println(c);
     }
 }
